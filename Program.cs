1702cdf4e2ad4c93ebaf00e81f927ec5ba1f82b3
AlexandrKazin <pil9rims@yandex.ru>
2026-04-15 16:28:10 +0300

    Implement basic scraping logic

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+
+string url = "https://example.com/news";
+using HttpClient client = new HttpClient();
+string html = await client.GetStringAsync(url);
+
+HtmlDocument doc = new HtmlDocument();
+doc.LoadHtml(html);
+
+var titles = doc.DocumentNode.SelectNodes("//h2[@class='title']");
+if (titles != null)
+{
+    foreach (var node in titles)
+        Console.WriteLine(node.InnerText.Trim());
+}
+else
+{
+    Console.WriteLine("No titles found.");
+}
